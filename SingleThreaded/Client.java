9f46c1955c31660a3fec67a42c06c4e92aa8a3e9
yashkaskar1 <yashvilaskaskar@4gmail.com>
2025-07-11 21:25:11 +0530

    Initial commit

---
+++ SingleThreaded/Client.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+    
+    public void run() throws UnknownHostException, IOException{
+        int port = 8080;
+        InetAddress address = InetAddress.getByName("localhost");
+        Socket socket = new Socket(address, port);
+        PrintWriter toSocket = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        toSocket.println("Hello World from socket "+socket.getLocalSocketAddress());
+        String line = fromSocket.readLine();
+        System.out.println("Response from the socket is:"+line);
+        toSocket.close();
+        fromSocket.close();
+        socket.close();
+    }
+    
+    public static void main(String[] args) {
+        
+        try{
+            Client client = new Client();
+            client.run();
+        } catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ SingleThreaded/Server.java
@@ -0,0 +1,45 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class Server {
+    
+    public void run() throws IOException{
+        int port = 8080;
+        ServerSocket socket = new ServerSocket(port);
+        socket.setSoTimeout(10000);
+        while(true){
+           try{
+             System.out.println("Server is listening on port: "+port);
+            Socket acceptedConnection = socket.accept();
+            System.out.println("Connected to "+acceptedConnection.getRemoteSocketAddress());
+            PrintWriter toClient = new PrintWriter(acceptedConnection.getOutputStream(), true);
+            BufferedReader fromClient = new BufferedReader(new InputStreamReader(acceptedConnection.getInputStream()));
+            toClient.println("Hello World from the server");
+            toClient.close();
+            fromClient.close();
+            acceptedConnection.close();
+            socket.close();
+           }
+           catch(IOException ex){
+            ex.printStackTrace();
+           }
+
+        }
+    }
+
+    public static void main(String[] args){
+        Server server = new Server();
+        try{
+            server.run();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+}
